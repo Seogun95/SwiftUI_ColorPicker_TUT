7f8dfd33ac822efb37788b75b960553914944857
SeonJungKim <ksj083895@gmail.com>
2021-02-07 16:08:52 +0500

    ColorPicker

--- SwiftUI_ColorPicker_TUT/ContentView.swift
+++ SwiftUI_ColorPicker_TUT/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+    @State private var bgColor =
+        Color(.sRGB, red: 0.98, green: 0.9, blue: 0.5)
 
+        var body: some View {
+            VStack {
+                ColorPicker("배경화면 색상 선택", selection: $bgColor)
+                    .padding()
+            }
+            //frame은 반드시 background 앞에 와야 한다.
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(bgColor)
+            .edgesIgnoringSafeArea(.all)
+        }
+    }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
